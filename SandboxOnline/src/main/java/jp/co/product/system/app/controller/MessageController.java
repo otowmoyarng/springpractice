3992ba7a95d7e05954a58e4fbce8d12c92c11a32
hidemusiam <hidemusiam@gmail.com>
2019-11-21 10:57:09 +0900

    メッセージボックスの表示プログラム検証中

---
+++ SandboxOnline/src/main/java/jp/co/product/system/app/controller/MessageController.java
@@ -0,0 +1,32 @@
+package jp.co.product.system.app.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/message")
+public class MessageController extends ProductBaseConroller {
+
+	@Override
+	protected Class<?> getInnertype() {
+		return this.getClass();
+	}
+
+	@RequestMapping(value = "/")
+	public String init() {
+		return "message";
+	}
+	
+	@RequestMapping(value = "show/{errorflg}", method = RequestMethod.GET)
+	public String message(Model model, @PathVariable("errorflg") String errorflg, Map<String,String> map) {
+		logger.info("errorflg:" + errorflg);
+		model.addAttribute("errorflg", errorflg == null ? "-1" : errorflg);
+		map.put("errorflg", errorflg == null ? "-1" : errorflg);
+		return "message";
+	}
+}
