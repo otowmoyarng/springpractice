4b8b9dc787083796571ae25dd19f664d0a275b7d
hidemusiam <hidemusiam@gmail.com>
2019-12-03 08:26:01 +0900

    画面遷移パラメータ

--- SandboxOnline/src/main/webapp/resources/js/appcommon.js
+++ SandboxOnline/src/main/webapp/resources/js/appcommon.js
@@ -15,17 +15,26 @@ function showwindow() {
 /**
  * 検索ダイアログ表示
  */
-function showSearchDialog(companykbn, companyno_keyname) {
+function showSearchDialog(companykbn) {
 	
-	// 会社番号を取得する
-    var cno = document.getElementById(companyno_keyname);
+	// 会社区分を取得する
+    var kbn = document.getElementById('companykbn');
     
-    var searchparamters = {};
-    searchparamters.companyno = cno.value;
-    searchparamters.companykbn = companykbn;
+	// 会社番号を取得する
+    var cno = document.getElementById('companyno');
+    
+    var searchparamters = new Array(kbn.value, cno.value);
+    console.log('companykbn:' + kbn.value);
+    console.log('companyno:' + cno.value);
+//    var searchparamters = {};
+//    searchparamters.companyno = cno.value;
+//    searchparamters.companykbn = companykbn;
 //    window.alert('companyno:' + searchparamters.companyno);
 //    window.alert('companykbn:' + searchparamters.companykbn);
     
     var option = 'dialogheight=768; dialogwidth=1280;';
-    window.showModalDialog('./search/init', searchparamters, option);
+    var result = window.showModalDialog('./search/init', searchparamters, option);
+//    var result = window.showModalDialog('../../searchDialog.html', searchparamters, option);
+//    var result = window.showModalDialog('./searchDialog.html', searchparamters, option);
+    console.log('result:' + result);
 }
--- SandboxOnline/src/main/webapp/resources/js/searchDialog.js
+++ SandboxOnline/src/main/webapp/resources/js/searchDialog.js
@@ -1,16 +1,30 @@
 /**
  * 検索ダイアログ	JS定義
  */
- function getSelectedItms() {
-	 var tbody = docmennt.getElementByID('searchlist-tbody');
+function setParams() {
+	var params = window.dialogArguments;
+	
+	if (params != null) {
+		if (params[0] != null) {
+			console.log("params[0]:" + params[0]);
+			document.getElementById('companykbn' + params[0]).checked = 'checked';
+			document.getElementById('hdn_companykbn').value = params[0];
+		}
+		if (params[0] != null) {
+			console.log("params[0]:" + params[0]);
+		}
+	}
+}
+function getSelectedItms() {
+	var tbody = docmennt.getElementByID('searchlist-tbody');
 	 
-	 for (var i=0, rowlength=tbody.children.length; i < rowlength; i++) {
+	for (var i=0, rowlength=tbody.children.length; i < rowlength; i++) {
 		 
-		 for (var j=0, columnlength=tbody.children[i].cells.length; j < columnlength; i++) {
+		for (var j=0, columnlength=tbody.children[i].cells.length; j < columnlength; i++) {
 			 
-			 var cell = tbody.children[i].cells[j];
-			 console.log(i + "-" + i);
-			 console.log(cell);
-		 }
-	 }
- }
+			var cell = tbody.children[i].cells[j];
+			console.log(i + "-" + i);
+			console.log(cell);
+		}
+	}
+}
