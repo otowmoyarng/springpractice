461890df34dde0d3988355fdc517e22620bf3b26
hidemusiam <hidemusiam@gmail.com>
2019-12-04 08:41:03 +0900

    js修正

--- SandboxOnline/src/main/webapp/resources/js/appcommon.js
+++ SandboxOnline/src/main/webapp/resources/js/appcommon.js
@@ -15,7 +15,7 @@ function showwindow() {
 /**
  * 検索ダイアログ表示
  */
-function showSearchDialog(companykbn) {
+function showSearchDialog() {
 	
 	// 会社区分を取得する
     var kbn = document.getElementById('companykbn');
@@ -26,15 +26,28 @@ function showSearchDialog(companykbn) {
     var searchparamters = new Array(kbn.value, cno.value);
     console.log('companykbn:' + kbn.value);
     console.log('companyno:' + cno.value);
-//    var searchparamters = {};
-//    searchparamters.companyno = cno.value;
-//    searchparamters.companykbn = companykbn;
-//    window.alert('companyno:' + searchparamters.companyno);
-//    window.alert('companykbn:' + searchparamters.companykbn);
     
     var option = 'dialogheight=768; dialogwidth=1280;';
     var result = window.showModalDialog('./search/init', searchparamters, option);
-//    var result = window.showModalDialog('../../searchDialog.html', searchparamters, option);
-//    var result = window.showModalDialog('./searchDialog.html', searchparamters, option);
+    console.log('result:' + result);
+}
+
+/**
+ * 検索ダイアログ表示
+ */
+function showSearchDialogTest() {
+	
+	// 会社区分を取得する
+    var kbn = document.getElementById('companykbn');
+    
+	// 会社番号を取得する
+    var cno = document.getElementById('companyno');
+    
+    var searchparamters = new Array(kbn.value, cno.value);
+    console.log('companykbn:' + kbn.value);
+    console.log('companyno:' + cno.value);
+    
+    var option = 'dialogheight=768; dialogwidth=1280;';
+    var result = window.showModalDialog('./searchDialog.html', searchparamters, option);
     console.log('result:' + result);
 }
--- SandboxOnline/src/main/webapp/resources/js/searchDialog.js
+++ SandboxOnline/src/main/webapp/resources/js/searchDialog.js
@@ -1,6 +1,22 @@
 /**
  * 検索ダイアログ	JS定義
  */
+
+/**
+ * 初期化処理
+ */
+function initialize() {
+	
+	// 画面遷移パラメータ設定
+	setParams();
+	
+	// 選択ボタンの使用可否設定
+	enableSelectedButton();
+}
+
+/**
+ * 画面遷移パラメータ設定
+ */
 function setParams() {
 	var params = window.dialogArguments;
 	
@@ -10,21 +26,61 @@ function setParams() {
 			document.getElementById('companykbn' + params[0]).checked = 'checked';
 			document.getElementById('hdn_companykbn').value = params[0];
 		}
-		if (params[0] != null) {
-			console.log("params[0]:" + params[0]);
+		if (params[1] != null) {
+			console.log("params[1]:" + params[1]);
+			document.getElementById('companyno').value = params[1];
 		}
 	}
 }
-function getSelectedItms() {
-	var tbody = docmennt.getElementByID('searchlist-tbody');
-	 
-	for (var i=0, rowlength=tbody.children.length; i < rowlength; i++) {
-		 
-		for (var j=0, columnlength=tbody.children[i].cells.length; j < columnlength; i++) {
-			 
-			var cell = tbody.children[i].cells[j];
-			console.log(i + "-" + i);
+
+/**
+ * 選択ボタンクリック使用可否
+ */
+function enableSelectedButton() {
+	
+	var tablecount = searchlistmeisai.rows.length;
+    var button = document.getElementById('selectedbutton');
+    
+	button.disabled = !(tablecount > 0);
+}
+
+/**
+ * 選択ボタンクリック時
+ */
+function onclickSelectedButton() {
+//	var tbody = docmennt.getElementByID('searchlist-tbody');
+//	 
+//	for (var i=0, rowlength=tbody.children.length; i < rowlength; i++) {
+//		 
+//		for (var j=0, columnlength=tbody.children[i].cells.length; j < columnlength; i++) {
+//			 
+//			var cell = tbody.children[i].cells[j];
+//			console.log(i + "-" + i);
+//			console.log(cell);
+//		}
+//	}
+	for (var i=0, rowlength=searchlistmeisai.rows.length; i < rowlength; i++) {
+		
+		for (var j=0, columnlength=searchlistmeisai.rows[i].cells.length; j < columnlength; j++) {
+			
+			// j=0の場合はchildren[0]からinputタグを取得する
+			// 上記以外はchildrenを参照しない
+			// rowspan=2の行は奇数行しか取得できない
+			var cell;
+			if (j == 0) {
+				cell = searchlistmeisai.rows[i].cells[j].children[0];
+			} else {
+				cell = searchlistmeisai.rows[i].cells[j];
+			}
+			console.log(i + "-" + j);
 			console.log(cell);
 		}
 	}
+}
+
+/**
+ * 選択ボタンクリック時
+ */
+function onclickCancelButton() {
+	window.close();
 }
